2cc30b2c529dc78468a663e19467b1ac98690a94
tuncaytas <92614513+tuncaytas@users.noreply.github.com>
2022-02-14 20:27:32 -0600

    db added

---
+++ db/db.tf
@@ -0,0 +1,37 @@
+variable "region" {
+    default = "us-east-1"
+}
+module "cluster" {
+  source  = "terraform-aws-modules/rds-aurora/aws"
+
+  name           = "test-aurora-db-postgres96"
+  engine         = "aurora-postgresql"
+  engine_version = "11.12"
+  instance_class = "db.r6g.large"
+  instances = {
+    one = {}
+    3 = {
+      instance_class = "db.r6g.2xlarge"
+    }
+  }
+
+  vpc_id  = "vpc-12345678"
+  subnets = ["subnet-12345678", "subnet-87654321"]
+
+  allowed_security_groups = ["sg-12345678"]
+  allowed_cidr_blocks     = ["10.20.0.0/20"]
+
+  storage_encrypted   = true
+  apply_immediately   = true
+  monitoring_interval = 10
+
+  db_parameter_group_name         = "default"
+  db_cluster_parameter_group_name = "default"
+
+  enabled_cloudwatch_logs_exports = ["postgresql"]
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
